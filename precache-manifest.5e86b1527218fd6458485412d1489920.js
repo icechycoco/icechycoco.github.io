e2cff4c6aecb90d15e312c0c140822e7232a241c
icechycoco <icechycoco@gmail.com>
2020-04-04 10:20:31 +0700

    initial

---
+++ precache-manifest.5e86b1527218fd6458485412d1489920.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f34bd3ce014c55eed2930ae78ab9469a",
+    "url": "./index.html"
+  },
+  {
+    "revision": "136a0efa9f30f5a3d5ee",
+    "url": "./static/css/2.da6e3115.chunk.css"
+  },
+  {
+    "revision": "a5ed9c3b276622c86d15",
+    "url": "./static/css/main.29df2f9e.chunk.css"
+  },
+  {
+    "revision": "136a0efa9f30f5a3d5ee",
+    "url": "./static/js/2.ba452a3b.chunk.js"
+  },
+  {
+    "revision": "5f420ab85059e0c0ba087c0b8990bcd3",
+    "url": "./static/js/2.ba452a3b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a5ed9c3b276622c86d15",
+    "url": "./static/js/main.00170405.chunk.js"
+  },
+  {
+    "revision": "7304c6cd1204852fb3fb",
+    "url": "./static/js/runtime-main.1451e3c5.js"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "./static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "./static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "./static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "./static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "./static/media/glyphicons-halflings-regular.fa277232.woff"
+  },
+  {
+    "revision": "4512d9f1d3904d480ec131901bd22a6f",
+    "url": "./static/media/mall.4512d9f1.jpg"
+  },
+  {
+    "revision": "a3c41cd3b43bd0f2abc384cd13e17380",
+    "url": "./static/media/taxi.a3c41cd3.jpg"
+  }
+]);
